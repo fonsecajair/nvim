e37b3f4e6f717302e3e1b196841d2fa3994be330
Jair Lucas <jair.lucas.menezes@gmail.com>
2026-09-12 23:12:30 -0400

    Initial commit

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,25 @@
+-- ~/.config/nvim/lua/plugins/telescope.lua
+return {
+  "nvim-telescope/telescope.nvim",
+  version = "*",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+  },
+  keys = {
+    {
+      "<leader>ff",
+      function()
+        require("telescope.builtin").find_files()
+      end,
+      desc = "Find Files",
+    },
+    {
+      "<leader>fg",
+      function()
+        require("telescope.builtin").live_grep()
+      end,
+      desc = "Live Grep",
+    },
+  },
+}
